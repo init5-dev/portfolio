f30c24734902857e0f5da507adb1655c4fc54d88
Nelson Ochagavía <nelson.ochagavia@gmail.com>
2023-09-16 20:16:48 -0400

    Actualización de CSS

--- app/components/contactform.js
+++ app/components/contactform.js
@@ -49,47 +49,50 @@ const ContactForm = () => {
   }
 
   return (
-    <form
-      action="https://getform.io/f/5560cfc7-5e9d-40cc-b75b-e8ecb3146990"
-      method="POST"
-      className="w-full sm:mx-1 md:mx-0 lg:mx-0 sm:px-1 md:p-0 lg:p-0 fade-in-left-80"
-    >
-      <div className="w-full sm:mx-2 md:mx-0 lg:mx-0 pt-0 mb-3">
-        <input
-          type="text"
-          placeholder="Nombre"
-          name="name"
-          className="glass focus:outline-none relative w-full px-1 py-3 text-sm text-gray-600 placeholder-gray-400 bg-white border-0 rounded shadow outline-none"
-          required
-        />
-      </div>
-      <div className="w-full sm:mx-1 md:mx-0 lg:mx-0 pt-0 mb-3">
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          className="glass focus:outline-none relative w-full px-1 py-3 text-sm text-gray-600 placeholder-gray-400 bg-white border-0 rounded shadow outline-none"
-          required
-        />
-      </div>
-      <div className="sm:mx-2 md:mx-0 lg:mx-0 pt-0 mb-3">
-        <textarea
-          placeholder="Mensaje"
-          rows='10'
-          name="message"
-          className="resize-none glass focus:outline-none relative w-full px-1 py-3 text-sm text-gray-600 placeholder-gray-400 bg-white border-0 rounded shadow outline-none"
-          required
-        />
-      </div>
-      <div className="w-full sm:mx-2 md:mx-0 lg:mx-0 pt-0 mb-1 flex justify-center">
-        <button
-          className="active:bg-lime-600 hover:shadow-lg focus:outline-none px-6 py-3 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-lime-700 rounded shadow outline-none"
-          type="submit"
-        >
-          Enviar
-        </button>
-      </div>
-    </form>
+    <div>
+      <form
+        action="https://getform.io/f/5560cfc7-5e9d-40cc-b75b-e8ecb3146990"
+        method="POST"
+        className=""
+      >
+        <div className="w-full sm:mx-2 md:mx-0 lg:mx-0 pt-0 mb-3">
+          <input
+            type="text"
+            placeholder="Nombre"
+            name="name"
+            className="glass focus:outline-none relative w-full px-1 py-3 text-sm text-gray-600 placeholder-gray-400 bg-white border-0 rounded shadow outline-none"
+            required
+          />
+        </div>
+        <div className="w-full sm:mx-1 md:mx-0 lg:mx-0 pt-0 mb-3">
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            className="glass focus:outline-none relative w-full px-1 py-3 text-sm text-gray-600 placeholder-gray-400 bg-white border-0 rounded shadow outline-none"
+            required
+          />
+        </div>
+        <div className="sm:mx-2 md:mx-0 lg:mx-0 pt-0 mb-3">
+          <textarea
+            placeholder="Mensaje"
+            rows='10'
+            name="message"
+            className="resize-none glass focus:outline-none relative w-full px-1 py-3 text-sm text-gray-600 placeholder-gray-400 bg-white border-0 rounded shadow outline-none"
+            required
+          />
+        </div>
+        <div className="w-full sm:mx-2 md:mx-0 lg:mx-0 pt-0 mb-1 flex justify-center">
+          <button
+            className="active:bg-lime-600 hover:shadow-lg focus:outline-none px-6 py-3 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-lime-700 rounded shadow outline-none"
+            type="submit"
+          >
+            Enviar
+          </button>
+        </div>
+      </form>
+    </div>
+
   );
 };
 
